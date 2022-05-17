c61904c8f20a7458aac267bd668cceaf6e1c1e01
Karimov Emomali Abduevich <emomali17012000@gmail.com>
2022-05-17 12:01:54 +0300

    practika

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,40 @@
 export function romanToInteger(str) {
-    let result = 0;
     /*
     Напишите код, преобразующий число из римской записи в арабскую.
     На вход вы получите строку, ответом должно быть число.
      */
+    var result = 0;
+    var ema = 0;
+    var qwer = 0;
+    var asd;
+    for (var i = 0; i < str.length; i++) {
+        asd = ema;
+        switch (str[i]) {
+            case 'I':
+                ema = 1;
+                break;
+            case 'V':
+                ema = 5;
+                break;
+            case 'X':
+                ema = 10;
+                break;
+            case 'L':
+                ema = 50;
+                break;
+            case 'C':
+                ema = 100;
+                break;
+            case 'D':
+                ema = 500;
+                break;
+            case 'M':
+                ema = 1000;
+                break;
+        }
+        if (ema > asd) qwer = -2 * asd;
+        result = result + qwer + ema;
+        qwer = 0;
+    }
     return result;
 }
